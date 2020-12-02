78334be834ab19582d59ae611ceacd3720f78ff9
Edwin <edwin.celini@gmail.com>
2020-12-02 20:35:47 +0100

    feat: Create initial script and html

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const ip = require("ip");
+const address = ip.address();
+const port = 3000;
+
+app.use("/", express.static("public"));
+
+app.listen(port, address, () => {
+  console.log(`Example app listening at http://${address}:${port}`);
+});
