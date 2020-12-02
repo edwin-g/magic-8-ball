6d5fa54810f344ef45595349e6f9b21d8d804273
Edwin <edwin.celini@gmail.com>
2020-12-02 20:47:25 +0100

    style: Change project structure

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const ip = require("ip");
 const address = ip.address();
 const port = 3000;
 
-app.use("/", express.static("public"));
+// app.use("/", express.static("public"));
+app.use("/", express.static("./"));
 
 app.listen(port, address, () => {
   console.log(`Example app listening at http://${address}:${port}`);
